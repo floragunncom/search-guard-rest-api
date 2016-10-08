0ca342ddec5a5612b472f962bfe386e2e71fa689
Jochen Kressin <jkressin@floragunn.com>
2016-10-08 21:01:57 +0200

    license info added, removed jackson from pom

--- src/main/java/com/floragunn/searchguard/dlic/rest/api/RolesApiAction.java
+++ src/main/java/com/floragunn/searchguard/dlic/rest/api/RolesApiAction.java
@@ -1,5 +1,19 @@
 package com.floragunn.searchguard.dlic.rest.api;
 
+/*
+ * Copyright 2016 by floragunn UG (haftungsbeschränkt) - All rights reserved
+ * 
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed here is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * 
+ * This software is free of charge for non-commercial and academic use. 
+ * For commercial use in a production environment you have to obtain a license 
+ * from https://floragunn.com
+ * 
+ */
+
 import org.elasticsearch.client.Client;
 import org.elasticsearch.cluster.ClusterService;
 import org.elasticsearch.common.bytes.BytesReference;
